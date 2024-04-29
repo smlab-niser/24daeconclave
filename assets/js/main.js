b8969fbd5524d36ef53b00b538c2e6f9750eba41
smlab-niser <smlab@niser.ac.in>
2024-04-29 14:45:37 +0530

    Updated with changes.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+
+
+var map = L.map("map").setView([20.17146235823554, 85.68426322527682], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
+
+
+
+
+L.marker([20.17146235823554, 85.68426322527682])
+    
+    .addTo(map)
+    .bindTooltip("<strong>Pathani Samanta Auditorium, NISER</strong>");
+    
+
+
+
+
